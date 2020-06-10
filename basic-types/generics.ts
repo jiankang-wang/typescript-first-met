8f79f66b0bd3f58474453316dc4f5671211d763b
wangjiankang <1159085983@qq.com>
2020-06-10 20:30:56 +0800

    feat: interfaceAndClassOfgengerics

--- basic-types/generics.ts
+++ basic-types/generics.ts
@@ -23,6 +23,54 @@ function echoWidthLength<T extends IWithLength>(arg: T): T {
   return arg
 }
 
-const resultThree = echoWidthLength('123')
-const obj = echoWidthLength({length: 10})
-const arr = echoWidthLength([1, 2, 3])
+// const resultThree = echoWidthLength('123')
+// const obj = echoWidthLength({length: 10})
+// const arr = echoWidthLength([1, 2, 3])
+
+// 4: 类的泛型
+class Quere<T> {
+  private data = []
+  push(item: T) {
+    return this.data.push(item)
+  }
+  pop(): T {
+    return this.data.shift()
+  }
+}
+
+const quere = new Quere<number>()
+quere.push(2)
+
+// 5: interface 泛型类
+interface IKeyPare<T, U> {
+  key: T,
+  value: U
+}
+
+let kp1:IKeyPare<number, string> = {
+  key: 1,
+  value: 'test'
+}
+
+let arrThree: number[] = [2, 2, 3]
+let arrayFour: Array<number> = [1, 2, 3]
+
+// 方法的泛型
+interface IPlus<T> {
+  (a: T, b: T): T
+}
+
+function plus(a: number, b: number) {
+  return a + b
+}
+
+function plusStr(a: string, b: string) {
+  return `${a}~${b}`
+}
+
+const a: IPlus<number> = plus
+const b : IPlus<string> = plusStr
+console.log(a(1, 2))
+console.log(b('1', '2'))
+
+
